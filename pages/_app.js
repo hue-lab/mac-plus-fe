eacbf8f1eb4b2bfdfd2bfbb72c27bb0637cdab97
AntonioPivaldi <9212565@gmail.com>
2023-08-20 15:48:47 +0300

    feat: add basic routing and translations

--- pages/_app.js
+++ pages/_app.js
@@ -13,52 +13,52 @@ import { currentDemo } from '~/server/queries';
 import "~/public/sass/style.scss";
 
 const App = ({ Component, pageProps }) => {
-    const store = useStore();
+  const store = useStore();
 
-    useEffect(() => {
-        if (store.getState().demo.current !== currentDemo) {
-            store.dispatch(demoActions.refreshStore(currentDemo));
-        }
-    }, [])
+  useEffect(() => {
+    if (store.getState().demo.current !== currentDemo) {
+      store.dispatch(demoActions.refreshStore(currentDemo));
+    }
+  }, [])
 
-    return (
-        <Provider store={store}>
-            <PersistGate
-                persistor={store.__persistor}
-                loading={<div className="loading-overlay">
-                    <div className="bounce-loader">
-                        <div className="bounce1"></div>
-                        <div className="bounce2"></div>
-                        <div className="bounce3"></div>
-                        <div className="bounce4"></div>
-                    </div>
-                </div>}>
-                <Helmet>
-                    <meta charSet="UTF-8" />
-                    <meta http-equiv="X-UA-Compatible" content="IE=edge" />
-                    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no" />
+  return (
+    <Provider store={store}>
+      <PersistGate
+        persistor={store.__persistor}
+        loading={<div className="loading-overlay">
+          <div className="bounce-loader">
+            <div className="bounce1"></div>
+            <div className="bounce2"></div>
+            <div className="bounce3"></div>
+            <div className="bounce4"></div>
+          </div>
+        </div>}>
+        <Helmet>
+          <meta charSet="UTF-8" />
+          <meta http-equiv="X-UA-Compatible" content="IE=edge" />
+          <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no" />
 
-                    <title>Riode - React eCommerce Template</title>
+          <title>Mac Plus</title>
 
-                    <meta name="keywords" content="React Template" />
-                    <meta name="description" content="Riode - React eCommerce Template" />
-                    <meta name="author" content="D-THEMES" />
-                </Helmet>
+          <meta name="keywords" content="React Template" />
+          <meta name="description" content="Riode - React eCommerce Template" />
+          <meta name="author" content="D-THEMES" />
+        </Helmet>
 
-                <Layout>
-                    <Component {...pageProps} />
-                </Layout>
-            </PersistGate>
-        </Provider>
-    );
+        <Layout>
+          <Component {...pageProps} />
+        </Layout>
+      </PersistGate>
+    </Provider>
+  );
 }
 
 App.getInitialProps = async ({ Component, ctx }) => {
-    let pageProps = {};
-    if (Component.getInitialProps) {
-        pageProps = await Component.getInitialProps(ctx);
-    }
-    return { pageProps };
+  let pageProps = {};
+  if (Component.getInitialProps) {
+    pageProps = await Component.getInitialProps(ctx);
+  }
+  return { pageProps };
 };
 
 export default wrapper.withRedux(App);
