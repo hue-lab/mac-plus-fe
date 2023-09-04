c5e6d51749e0a0016f7b6a14f9fcf9ef39f9c227
Zidiks <zidiks228@gmail.com>
2023-09-04 20:53:41 +0300

    fix: some issues

--- components/common/partials/main-menu.jsx
+++ components/common/partials/main-menu.jsx
@@ -1,13 +1,9 @@
 import { useRouter } from 'next/router';
-import { useState } from 'react';
 
 import ALink from '~/components/features/custom-link';
 
-import { mainMenu } from '~/utils/data/menu';
-
 function MainMenu({ router }) {
-  const pathname = useRouter().pathname;
-  const [currentRoute, setCurrentRoute] = useState(0 || currentRoute);
+  const { pathname, query, asPath, route } = useRouter();
   const navBar = [
     {
       name: 'Главная',
@@ -15,49 +11,49 @@ function MainMenu({ router }) {
     },
     {
       name: 'Mac',
-      href: '/shop',
+      href: '/shop/all/',
       children: [
         {
-          name: 'Mac',
-          href: '/shop',
+          name: 'MacBook',
+          href: '/shop/macbook/',
         },
       ]
     },
     {
       name: 'iPhone',
-      href: '/shop',
+      href: '/shop/iphone/',
     },
     {
       name: 'iPad',
-      href: '/shop',
+      href: '/shop/ipad/',
     },
     {
       name: 'Watch',
-      href: '/shop',
+      href: '/shop/watch/',
     },
     {
       name: 'AirPods',
-      href: '/shop',
+      href: '/shop/airpods/',
     },
   ];
 
   return (
     <nav className="main-nav ml-4">
       <ul className="menu">{navBar.map((route, index) => {
+        console.log(asPath);
         return (
-          <li className={`${route.children?.length ? 'submenu' : ''} ${pathname === route.href ? 'active' : ''}`} key={route.name + index}>
+          <li className={`${route.children?.length ? 'submenu' : ''} ${asPath === route.href ? 'active' : ''}`} key={route.name + index}>
             <ALink href={route.href}>{route.name}</ALink>
 
             { route.children?.length && <div className="megamenu" style={{ marginLeft: "-19px" }}>
               <div className="row">
-                <div className="col-6">
-                  <h4 className="menu-title">Variations 1</h4>
+                <div className="col-5">
                   <ul>
                     {
-                      mainMenu.shop.variation1.map((item, index) => (
-                        <li key={`shop-${item.title}`}>
-                          <ALink href={'/' + item.url}>
-                            {item.title}
+                      route.children.map((item, index) => (
+                        <li key={`shop-${item.name + index}`}>
+                          <ALink href={'/' + item.href}>
+                            {item.name}
                             {item.hot ? <span className="tip tip-hot">Hot</span> : ""}
                           </ALink>
                         </li>
@@ -66,7 +62,7 @@ function MainMenu({ router }) {
                   </ul>
                 </div>
 
-                <div className="col-6 menu-banner menu-banner1 banner banner-fixed">
+                <div className="col-7 menu-banner menu-banner1 banner banner-fixed">
                   <figure>
                     <img src="./images/menu/banner-1.jpg" alt="Menu banner" width="221" height="330" />
                   </figure>
