8dd4165fd9717329e02887de1dfa7bb02fb885f6
Zidiks <zidiks228@gmail.com>
2023-09-04 21:28:34 +0300

    fix: link

--- pages/_document.js
+++ pages/_document.js
@@ -12,7 +12,7 @@ export default class MyDocument extends Document {
         <Head>
           <base href="/"></base>
           <title>Mac Plus</title>
-          <link rel="icon" href="images/icons/favicon.png" />
+          <link rel="icon" href="images/icons/favicon.ico" />
           <link rel="stylesheet" href="https://fonts.googleapis.com/css?family=Poppins:300,400,500,600,700,800,900" />
           <link rel="stylesheet" type="text/css" href="vendor/riode-fonts/riode-fonts.css" />
           <link rel="stylesheet" type="text/css" href="vendor/fontawesome-free/css/all.min.css" />
