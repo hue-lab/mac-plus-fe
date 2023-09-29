63679c5592fc9afac641fe35a460658865232b45
zidiks <zidiks228@gmail.com>
2023-09-29 12:33:14 +0300

    MACPLUS-16 | home page slides

--- pages/index.jsx
+++ pages/index.jsx
@@ -1,7 +1,6 @@
 import React from 'react';
 import Helmet from 'react-helmet';
 
-// import Home Components
 import IntroSection from '~/components/partials/home/intro-section';
 import CategorySection from '~/components/partials/home/category-section';
 import BannerSection from '~/components/partials/home/banner-section';
@@ -10,19 +9,22 @@ import BlogSection from '~/components/partials/home/blog-section';
 import { getCategoryTree } from "~/utils/endpoints/categoryTree";
 import { getLatestArticles } from '~/utils/endpoints/articles';
 import {getRecProducts} from "~/utils/endpoints/products";
+import {getSlides} from "~/utils/endpoints/slides";
 
 HomePage.getInitialProps = async (context) => {
   const categoryTree = await getCategoryTree();
   const articles = await getLatestArticles();
   const recProducts = await getRecProducts();
+  const slides = await getSlides();
   return {
     categoryTree: categoryTree.children,
     articles: articles.data,
-    recProducts : recProducts.data,
+    recProducts: recProducts.data,
+    slides: slides.data,
   };
 }
 
-export default function HomePage({ categoryTree, articles, recProducts }) {
+export default function HomePage({ articles, recProducts, slides }) {
 
 
   return (
@@ -34,7 +36,7 @@ export default function HomePage({ categoryTree, articles, recProducts }) {
       <h1 className="d-none">Mac Plus - Главная</h1>
 
       <div className="page-content">
-        <IntroSection />
+        <IntroSection slides={slides} />
 
         <CategorySection recProducts={recProducts} />
 
