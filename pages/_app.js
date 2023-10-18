788acd83147e2e71ce414a235bef8fd2b109d414
zidiks <zidiks228@gmail.com>
2023-10-18 17:29:36 +0300

    hot fix: ssr

--- pages/_app.js
+++ pages/_app.js
@@ -1,14 +1,9 @@
-import { useEffect } from 'react';
 import { useStore, Provider } from "react-redux";
 import { PersistGate } from 'redux-persist/integration/react';
 import Helmet from "react-helmet";
 
 import { wrapper } from "../store/index.js";
 import Layout from '~/components/layout';
-
-import { demoActions } from '~/store/demo';
-
-import { currentDemo } from '~/server/queries';
 
 import "~/public/sass/style.scss";
 import {getCategoryTree} from "~/utils/endpoints/categoryTree";
@@ -18,24 +13,9 @@ import {getMenuByCode} from "~/utils/endpoints/menu";
 const App = ({ Component, pageProps }) => {
   const store = useStore();
 
-  useEffect(() => {
-    if (store.getState().demo.current !== currentDemo) {
-      store.dispatch(demoActions.refreshStore(currentDemo));
-    }
-  }, [])
-
   return (
-    <Provider store={store}>
-      <PersistGate
-        persistor={store.__persistor}
-        loading={<div className="loading-overlay">
-          <div className="bounce-loader">
-            <div className="bounce1"></div>
-            <div className="bounce2"></div>
-            <div className="bounce3"></div>
-            <div className="bounce4"></div>
-          </div>
-        </div>}>
+    typeof window === "undefined" ?
+      <Provider store={store}>
         <Helmet>
           <meta charSet="UTF-8" />
           <meta http-equiv="X-UA-Compatible" content="IE=edge" />
@@ -51,8 +31,36 @@ const App = ({ Component, pageProps }) => {
         <Layout categoryTree={pageProps.categoryTree} layoutFields={pageProps.layoutFields} footerNav={pageProps.footerNav}>
           <Component {...pageProps} />
         </Layout>
-      </PersistGate>
-    </Provider>
+      </Provider>
+      :
+      <Provider store={store}>
+        <PersistGate
+          persistor={store.__persistor}
+          loading={<div className="loading-overlay">
+            <div className="bounce-loader">
+              <div className="bounce1"></div>
+              <div className="bounce2"></div>
+              <div className="bounce3"></div>
+              <div className="bounce4"></div>
+            </div>
+          </div>}>
+          <Helmet>
+            <meta charSet="UTF-8" />
+            <meta http-equiv="X-UA-Compatible" content="IE=edge" />
+            <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no" />
+
+            <title>Mac Plus</title>
+
+            <meta name="keywords" content="React Template" />
+            <meta name="description" content="Riode - React eCommerce Template" />
+            <meta name="author" content="D-THEMES" />
+          </Helmet>
+
+          <Layout categoryTree={pageProps.categoryTree} layoutFields={pageProps.layoutFields} footerNav={pageProps.footerNav}>
+            <Component {...pageProps} />
+          </Layout>
+        </PersistGate>
+      </Provider>
   );
 }
 
