040da778e46fdf70b45f92882130c5abe561f35a
zidiks <zidiks228@gmail.com>
2024-01-24 17:06:39 +0300

    upd: add new yandex analytics code

--- pages/_document.js
+++ pages/_document.js
@@ -53,15 +53,15 @@ export default class MyDocument extends Document {
           <script id="yandex-analytics"
                   dangerouslySetInnerHTML={{
                     __html: `
-              (function (m, e, t, r, i, k, a){m[i] = m[i] || function () {
-                (m[i].a = m[i].a || []).push(arguments)
-              };
+                (function (m, e, t, r, i, k, a){m[i] = m[i] || function () {
+                  (m[i].a = m[i].a || []).push(arguments)
+                };
                 m[i].l=1*new Date();
                 for (var j = 0; j < document.scripts.length; j++) {if (document.scripts[j].src === r) {return;}}
                 k=e.createElement(t),a=e.getElementsByTagName(t)[0],k.async=1,k.src=r,a.parentNode.insertBefore(k,a)})
                 (window, document, "script", "https://mc.yandex.ru/metrika/tag.js", "ym");
       
-                ym(96220740, "init", {
+                ym(96232936, "init", {
                 clickmap:true,
                 trackLinks:true,
                 accurateTrackBounce:true,
@@ -73,7 +73,7 @@ export default class MyDocument extends Document {
           />
 
           <noscript>
-            <div><img src="https://mc.yandex.ru/watch/96220740" style={{position: 'absolute', left: '-9999px'}} alt=""/>
+            <div><img src="https://mc.yandex.ru/watch/96232936" style={{position: 'absolute', left: '-9999px'}} alt=""/>
             </div>
           </noscript>
         </body>
