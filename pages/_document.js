dbb73d5df8ef964db1fce1a8b365929cef15470a
Zidiks <zidiks228@gmail.com>
2024-02-12 20:11:56 +0300

    upd: metrics

--- pages/_document.js
+++ pages/_document.js
@@ -30,6 +30,38 @@ export default class MyDocument extends Document {
               `
                   }}
           />
+          <script id="google-tag-manager"
+                  dangerouslySetInnerHTML={{
+                    __html: `
+                (function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
+                new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
+                j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
+                'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
+                })(window,document,'script','dataLayer','GTM-MNFW8ZCH');
+              `
+                  }}
+          />
+          <script id="yandex-analytics"
+                  dangerouslySetInnerHTML={{
+                    __html: `
+                (function(m,e,t,r,i,k,a){m[i]=m[i]||function(){(m[i].a=m[i].a||[]).push(arguments)};
+                 m[i].l=1*new Date();
+                 for (var j = 0; j < document.scripts.length; j++) {if (document.scripts[j].src === r) { return; }}
+                 k=e.createElement(t),a=e.getElementsByTagName(t)[0],k.async=1,k.src=r,a.parentNode.insertBefore(k,a)})
+                 (window, document, "script", "https://mc.yandex.ru/metrika/tag.js", "ym");
+              
+                 ym(96220740, "init", {
+                      clickmap:true,
+                      trackLinks:true,
+                      accurateTrackBounce:true,
+                      webvisor:true
+                 });
+            `
+                  }}
+          />
+          <noscript>
+            <div><img src="https://mc.yandex.ru/watch/96220740" style={{position: 'absolute', left: '-9999px'}} alt=""/></div>
+          </noscript>
           <base href="/"></base>
           <title>Mac Plus</title>
           <link rel="icon" href="images/icons/favicon.ico"/>
@@ -49,36 +81,13 @@ export default class MyDocument extends Document {
           <iframe src="https://www.googletagmanager.com/ns.html?id=GTM-KZX7JHJG" height="0" width="0"
                   style={{display: 'none', visibility: 'hidden'}}></iframe>
         </noscript>
+        <noscript>
+          <iframe src="https://www.googletagmanager.com/ns.html?id=GTM-MNFW8ZCH" height="0" width="0"
+                  style={{display: 'none', visibility: 'hidden'}}></iframe>
+        </noscript>
 
         <Main/>
         <NextScript/>
-
-        <script id="yandex-analytics"
-                dangerouslySetInnerHTML={{
-                  __html: `
-                (function (m, e, t, r, i, k, a){m[i] = m[i] || function () {
-                  (m[i].a = m[i].a || []).push(arguments)
-                };
-                m[i].l=1*new Date();
-                for (var j = 0; j < document.scripts.length; j++) {if (document.scripts[j].src === r) {return;}}
-                k=e.createElement(t),a=e.getElementsByTagName(t)[0],k.async=1,k.src=r,a.parentNode.insertBefore(k,a)})
-                (window, document, "script", "https://mc.yandex.ru/metrika/tag.js", "ym");
-      
-                ym(96232936, "init", {
-                clickmap:true,
-                trackLinks:true,
-                accurateTrackBounce:true,
-                webvisor:true,
-                ecommerce:"dataLayer"
-              });
-            `
-                }}
-        />
-
-        <noscript>
-          <div><img src="https://mc.yandex.ru/watch/96232936" style={{position: 'absolute', left: '-9999px'}} alt=""/>
-          </div>
-        </noscript>
         </body>
       </Html>
     )
