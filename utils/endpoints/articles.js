42545a5e0417b01548a242b6d92a8e3600a71e65
AntonioPivaldi <9212565@gmail.com>
2024-02-27 20:58:58 +0300

    MACPLUS-74 | feat: not load posts with hidden modifier

--- utils/endpoints/articles.js
+++ utils/endpoints/articles.js
@@ -2,22 +2,23 @@ export async function getArticles(page = 1, limit = 8) {
   const queryParams = new URLSearchParams({
     page,
     limit,
+    hidden: false,
   }).toString();
   const res = await fetch(process.env.API_HOST + `/article?${queryParams}`);
-  return await res.json() || [];
+  return (await res.json()) || [];
 }
 
 export async function getLatestArticles(limit = 4) {
-  const res = await fetch(process.env.API_HOST + `/article?limit=${limit}`);
-  return await res.json() || [];
+  const res = await fetch(process.env.API_HOST + `/article?limit=${limit}&hidden=false`);
+  return (await res.json()) || [];
 }
 
 export async function getArticleById(id) {
-  const res = await fetch(process.env.API_HOST + '/article/' + id);
-  return await res.json() || [];
+  const res = await fetch(process.env.API_HOST + "/article/" + id);
+  return (await res.json()) || [];
 }
 
 export async function getArticleBySlug(slug) {
   const res = await fetch(process.env.API_HOST + `/article/item?slug=${slug}`);
-  return await res.json() || [];
+  return (await res.json()) || [];
 }
