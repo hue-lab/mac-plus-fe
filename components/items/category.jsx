b9149ee200290b903da185313bb8f74f306a52c9
AntonioPivaldi <9212565@gmail.com>
2024-03-12 19:12:28 +0300

    MACPLUS-76 | feat: add seo headers for categories and products

--- components/items/category.jsx
+++ components/items/category.jsx
@@ -1,57 +1,107 @@
-import React, {useEffect} from 'react';
-import Helmet from 'react-helmet';
-import ALink from '~/components/features/custom-link';
-import SidebarFilterOne from '~/components/partials/shop/sidebar/sidebar-filter-one'
-import ProductListOne from '~/components/partials/shop/product-list/product-list-one';
-import {getImgPath} from "~/utils";
+import React, { useEffect } from "react";
+import Helmet from "react-helmet";
+import ALink from "~/components/features/custom-link";
+import SidebarFilterOne from "~/components/partials/shop/sidebar/sidebar-filter-one";
+import ProductListOne from "~/components/partials/shop/product-list/product-list-one";
+import { getImgPath } from "~/utils";
 
-export default function Category({ banner, products, filters, category, page, filterObject, fullPath }) {
+export default function Category({
+  banner,
+  products,
+  filters,
+  category,
+  page,
+  filterObject,
+  fullPath,
+}) {
   useEffect(() => {
-    if (typeof window !== 'undefined') {
-      window.scrollTo({top: 0, behavior: 'smooth'});
+    if (typeof window !== "undefined") {
+      window.scrollTo({ top: 0, behavior: "smooth" });
     }
   }, [page, filters]);
+
+  const appliedFiltersKeys = Object.keys(filterObject);
+  const appliedFilters = appliedFiltersKeys.map((key) => {
+    const filter = filters.find((filter) => filter.code === key);
+    const appliedFilter = { name: filter.name, code: filter.code, _id: filter._id };
+    appliedFilter.options = filter.options.filter((option) =>
+      filterObject[key].includes(option.key)
+    );
+    return appliedFilter;
+  });
+
+  const filtersString = `${appliedFilters
+    .map((filter) => `${filter.name} - ${filter.options.map((option) => option.value).join(", ")}`)
+    .join("; ")}`;
+
+  const headerString = `${category.title}${filtersString.length ? ": " : ""}${filtersString}${
+    page > 1 ? `; Страница №${page}` : ""
+  }`;
 
   return (
     <main className="main bt-lg-none shop">
       <Helmet>
-        <title>{ category.title ||  category.name || 'Каталог' }</title>
-        <meta property="og:title" content={category.title ||  category.name || 'Каталог'} />
-        <meta name="description" content={category.description || category.name || ''} />
-        <meta property="og:description" content={category.description || category.name || ''} />
-        <meta name="keywords" content={(category.keywords || []).join(', ')} />
+        <title>{category.title || category.name || "Каталог"}</title>
+        <meta property="og:title" content={category.title || category.name || "Каталог"} />
+        <meta name="description" content={category.description || category.name || ""} />
+        <meta property="og:description" content={category.description || category.name || ""} />
+        <meta name="keywords" content={(category.keywords || []).join(", ")} />
       </Helmet>
 
-      <h1 className="d-none">Mac Plus - { category.name || 'Каталог' }</h1>
+      <h1 className="d-none">{headerString}</h1>
 
       <div className="page-content mb-10 pb-2">
         <div className="container">
           <ul className="breadcrumb breadcrumb-sm">
-            <li><ALink href="/"><i className="d-icon-home"></i></ALink></li>
-            <li><ALink href="/shop">Каталог</ALink></li>
-            { category?.name && <li>{ category.name }</li> }
+            <li>
+              <ALink href="/">
+                <i className="d-icon-home"></i>
+              </ALink>
+            </li>
+            <li>
+              <ALink href="/shop">Каталог</ALink>
+            </li>
+            {category?.name && <li>{category.name}</li>}
           </ul>
 
           <div className="row gutter-lg main-content-wrap">
             <SidebarFilterOne filters={filters} type="banner" filterObject={filterObject} />
 
             <div className="col-lg-9 main-content">
-              { banner && <div className="shop-banner banner" title={banner.seo?.seoImageAlt || ''} style={{ backgroundImage: `url('${getImgPath(banner.media)}')`, backgroundColor: "#f2f2f3" }}>
-                <div className="banner-content">
-                  <h4
-                    className="banner-subtitle d-inline-block mb-2 text-uppercase ls-normal bg-dark"
-                    style={{ color: 'white', padding: '.5rem 1rem' }}
-                  >{ banner.description }</h4>
-                  <h1 className="banner-title font-weight-bold ls-normal text-uppercase">{ banner.title }</h1>
-                  <ALink href={`/blog/${banner.seo?.seoUrl || '#'}`} className="btn btn-outline btn-dark btn-rounded">Подробнее</ALink>
+              {banner && (
+                <div
+                  className="shop-banner banner"
+                  title={banner.seo?.seoImageAlt || ""}
+                  style={{
+                    backgroundImage: `url('${getImgPath(banner.media)}')`,
+                    backgroundColor: "#f2f2f3",
+                  }}
+                >
+                  <div className="banner-content">
+                    <h4
+                      className="banner-subtitle d-inline-block mb-2 text-uppercase ls-normal bg-dark"
+                      style={{ color: "white", padding: ".5rem 1rem" }}
+                    >
+                      {banner.description}
+                    </h4>
+                    <h1 className="banner-title font-weight-bold ls-normal text-uppercase">
+                      {banner.title}
+                    </h1>
+                    <ALink
+                      href={`/blog/${banner.seo?.seoUrl || "#"}`}
+                      className="btn btn-outline btn-dark btn-rounded"
+                    >
+                      Подробнее
+                    </ALink>
+                  </div>
                 </div>
-              </div> }
+              )}
 
               <ProductListOne products={products} type="banner" fullPath={fullPath} />
             </div>
           </div>
         </div>
       </div>
-    </main >
-  )
+    </main>
+  );
 }
--- components/items/product.jsx
+++ components/items/product.jsx
@@ -1,65 +1,84 @@
-import React from 'react';
-import Helmet from 'react-helmet';
+import React from "react";
+import Helmet from "react-helmet";
 
-import ALink from '~/components/features/custom-link';
-import MediaFive from '~/components/partials/product/media/media-five';
-import DetailThree from '~/components/partials/product/detail/detail-three';
-import DescOne from '~/components/partials/product/desc/desc-one';
-import ProductSidebar from '~/components/partials/product/product-sidebar';
-import {getImgPath} from "~/utils";
+import ALink from "~/components/features/custom-link";
+import MediaFive from "~/components/partials/product/media/media-five";
+import DetailThree from "~/components/partials/product/detail/detail-three";
+import DescOne from "~/components/partials/product/desc/desc-one";
+import ProductSidebar from "~/components/partials/product/product-sidebar";
+import { getImgPath } from "~/utils";
 
 export default function ProductItem({ product, featured, deliveryMethods }) {
-
-  if (!product) return '';
+  if (!product) return "";
 
   const ogImage = product.seo?.seoImage[0];
+  const headerString = `${product.name} - купить в Минске. Интернет-магазин Macplus`;
 
   return (
     <main className="main single-product">
       <Helmet>
-        <title>{product.seo?.seoTitle || product.name || 'Mac Plus'}</title>
-        <meta property="og:title" content={product.seo?.seoTitle || product.name || 'Mac Plus'} />
-        <meta name="description" content={product.seo?.seoDescription || product.name || ''} />
-        <meta property="og:description" content={product.seo?.seoDescription || product.name || ''} />
-        <meta name="keywords" content={product.seo?.seoKeywords?.join(', ')} />
+        <title>{product.seo?.seoTitle || product.name || "Mac Plus"}</title>
+        <meta property="og:title" content={product.seo?.seoTitle || product.name || "Mac Plus"} />
+        <meta name="description" content={product.seo?.seoDescription || product.name || ""} />
+        <meta
+          property="og:description"
+          content={product.seo?.seoDescription || product.name || ""}
+        />
+        <meta name="keywords" content={product.seo?.seoKeywords?.join(", ")} />
         {ogImage && <meta property="og:image" content={getImgPath(ogImage.imageName)} />}
       </Helmet>
 
-      <h1 className="d-none">Mac Plus - {product.name}</h1>
+      <h1 className="d-none">{headerString}</h1>
 
-      {
-        !!product ?
-          <div className={`page-content mb-8`}>
-            <div className="container skeleton-body">
-              <div className="product-navigation">
-                <ul className="breadcrumb breadcrumb-lg">
-                  <li><ALink href="/"><i className="d-icon-home"></i></ALink></li>
-                  <li><ALink href="/shop" className="active">Каталог</ALink></li>
-                  <li><ALink href={`/${product.category.handle}/`} className="active">{ product.category.name }</ALink></li>
-                  <li>{product.name}</li>
-                </ul>
-              </div>
+      {!!product ? (
+        <div className={`page-content mb-8`}>
+          <div className="container skeleton-body">
+            <div className="product-navigation">
+              <ul className="breadcrumb breadcrumb-lg">
+                <li>
+                  <ALink href="/">
+                    <i className="d-icon-home"></i>
+                  </ALink>
+                </li>
+                <li>
+                  <ALink href="/shop" className="active">
+                    Каталог
+                  </ALink>
+                </li>
+                <li>
+                  <ALink href={`/${product.category.handle}/`} className="active">
+                    {product.category.name}
+                  </ALink>
+                </li>
+                <li>{product.name}</li>
+              </ul>
+            </div>
 
-              <div className="row gutter-lg">
-                <ProductSidebar featured={featured?.data || []} deliveryMethods={deliveryMethods || []} />
+            <div className="row gutter-lg">
+              <ProductSidebar
+                featured={featured?.data || []}
+                deliveryMethods={deliveryMethods || []}
+              />
 
-                <div className="col-lg-9">
-                  <div className="product product-single row">
-                    <div className="col-md-6">
-                      <MediaFive product={product} adClass='pb-0' />
-                    </div>
-
-                    <div className="col-md-6">
-                      <DetailThree product={product} isNav={false} />
-                    </div>
+              <div className="col-lg-9">
+                <div className="product product-single row">
+                  <div className="col-md-6">
+                    <MediaFive product={product} adClass="pb-0" />
                   </div>
 
-                  <DescOne product={product} isDivider={false} className="mt-2 m-4" isGuide={false} />
+                  <div className="col-md-6">
+                    <DetailThree product={product} isNav={false} />
+                  </div>
                 </div>
+
+                <DescOne product={product} isDivider={false} className="mt-2 m-4" isGuide={false} />
               </div>
             </div>
-          </div> : ''
-      }
+          </div>
+        </div>
+      ) : (
+        ""
+      )}
     </main>
-  )
+  );
 }
