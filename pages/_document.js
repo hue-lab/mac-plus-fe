d3a7d0e40367183dde2e7b99a54c89f4847e885b
zidiks <zidiks228@gmail.com>
2024-04-08 16:42:34 +0300

    upd: add meta pixel

--- pages/_document.js
+++ pages/_document.js
@@ -1,10 +1,10 @@
-import Document, { NextScript, Head, Main, Html } from 'next/document';
+import Document, {NextScript, Head, Main, Html} from 'next/document';
 import Helmet from "react-helmet";
 
 export default class MyDocument extends Document {
   static async getInitialProps(ctx) {
     const initialProps = await Document.getInitialProps(ctx)
-    return { ...initialProps, helmet: Helmet.renderStatic() }
+    return {...initialProps, helmet: Helmet.renderStatic()}
   }
 
   get headTitle() {
@@ -65,6 +65,22 @@ export default class MyDocument extends Document {
             `
                   }}
           />
+          <script id="meta-pixel"
+                  dangerouslySetInnerHTML={{
+                    __html: `
+               !function(f,b,e,v,n,t,s)
+                {if(f.fbq)return;n=f.fbq=function(){n.callMethod?
+                n.callMethod.apply(n,arguments):n.queue.push(arguments)};
+                if(!f._fbq)f._fbq=n;n.push=n;n.loaded=!0;n.version='2.0';
+                n.queue=[];t=b.createElement(e);t.async=!0;
+                t.src=v;s=b.getElementsByTagName(e)[0];
+                s.parentNode.insertBefore(t,s)}(window, document,'script',
+                'https://connect.facebook.net/en_US/fbevents.js');
+                fbq('init', '1184434382543279');
+                fbq('track', 'PageView');
+            `
+                  }}
+          />
           <noscript>
             <div><img src="https://mc.yandex.ru/watch/96220740" style={{position: 'absolute', left: '-9999px'}} alt=""/>
             </div>
@@ -93,7 +109,10 @@ export default class MyDocument extends Document {
           <iframe src="https://www.googletagmanager.com/ns.html?id=GTM-MNFW8ZCH" height="0" width="0"
                   style={{display: 'none', visibility: 'hidden'}}></iframe>
         </noscript>
-
+        <noscript>
+          <img src="https://www.facebook.com/tr?id=1184434382543279&ev=PageView&noscript=1" height="1" width="1"
+               style={{display: 'none'}}/>
+        </noscript>
         <Main/>
         <NextScript/>
         </body>
