77126007e920e0b9e7b2734cd1d9d6fcf9b8b8df
Zidiks <zidiks228@gmail.com>
2024-10-26 21:47:13 +0300

    feat: add page loading state

--- pages/_app.js
+++ pages/_app.js
@@ -1,31 +1,32 @@
-import { useStore, Provider } from "react-redux";
+import {useStore, Provider} from "react-redux";
 import Helmet from "react-helmet";
 
-import { wrapper } from "~/store";
+import {wrapper} from "~/store";
 import Layout from "~/components/layout";
 
 import "~/public/sass/style.scss";
-import { getCategoryTree } from "~/utils/endpoints/categoryTree";
-import { getFieldsObject } from "~/utils/endpoints/fields";
-import { getMenuByCode } from "~/utils/endpoints/menu";
+import {getCategoryTree} from "~/utils/endpoints/categoryTree";
+import {getFieldsObject} from "~/utils/endpoints/fields";
+import {getMenuByCode} from "~/utils/endpoints/menu";
+import NextNProgress from 'nextjs-progressbar';
 import React from "react";
 
-const App = ({ Component, pageProps }) => {
+const App = ({Component, pageProps}) => {
   const store = useStore();
 
   return (
     <Provider store={store}>
       <Helmet>
-        <meta charSet="UTF-8" />
-        <meta http-equiv="X-UA-Compatible" content="IE=edge" />
-        <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no" />
+        <meta charSet="UTF-8"/>
+        <meta http-equiv="X-UA-Compatible" content="IE=edge"/>
+        <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no"/>
 
         <title>{pageProps.layoutFields["main-seo-title"] || "Mac Plus"}</title>
         <meta
           property="og:title"
           content={pageProps.layoutFields["main-seo-title"] || "Mac Plus"}
         />
-        <meta name="keywords" content="React Template" />
+        <meta name="keywords" content="React Template"/>
         <meta
           name="description"
           content={
@@ -40,9 +41,10 @@ const App = ({ Component, pageProps }) => {
             "Интернет-магазин электроники в Беларуси"
           }
         />
-        <meta name="author" content="D-THEMES" />
+        <meta name="author" content="D-THEMES"/>
       </Helmet>
 
+      <NextNProgress color="#007945" />
       <Layout
         categoryTree={pageProps.categoryTree}
         layoutFields={pageProps.layoutFields}
@@ -54,7 +56,7 @@ const App = ({ Component, pageProps }) => {
   )
 };
 
-App.getInitialProps = async ({ Component, ctx }) => {
+App.getInitialProps = async ({Component, ctx}) => {
   const categoryTree = await getCategoryTree();
   const footerNav = await getMenuByCode("footer_nav");
   const layoutFields = await getFieldsObject(
