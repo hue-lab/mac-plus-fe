caa757f992c357fa5c0d128ef2b087e0e746d973
Zidiks <zidiks228@gmail.com>
2024-10-29 23:53:42 +0300

    Revert "upd: some fixes"
    This reverts commit a6417b9adbcd918eaa56a78574f1347d6a27fcc7.

--- pages/_app.js
+++ pages/_app.js
@@ -54,166 +54,31 @@ const App = ({Component, pageProps}) => {
 };
 
 App.getInitialProps = async ({Component, ctx}) => {
-  // const [
-  //   categoryTree,
-  //   footerNav,
-  //   layoutFields,
-  // ] = await Promise.all([
-  //   getCategoryTree(),
-  //   getMenuByCode("footer_nav"),
-  //   getFieldsObject(
-  //     "telegram",
-  //     "viber",
-  //     "instagram",
-  //     "phone",
-  //     "email",
-  //     "address",
-  //     "work_time",
-  //     "copyright",
-  //     "legal",
-  //     "nav-sale-title",
-  //     "nav-sale-link",
-  //     "nav-sale-image",
-  //     "main-seo-title",
-  //     "main-seo-description",
-  //     "nav-limit"
-  //   )
-  // ]);
-  // console.log(categoryTree, footerNav, layoutFields);
   const [
     categoryTree,
     footerNav,
     layoutFields,
-  ] = [
-    {
-      _id: '65130f8c61d75bc4c35899e4',
-      name: 'root',
-      handle: 'root',
-      description: '',
-      media: [],
-      children: []
-    },
-    {
-      _id: '6527f364d9f2333094474f95',
-      name: 'Навигация [Footer]',
-      handle: '#',
-      description: '',
-      media: [],
-      children: [
-        {
-          _id: '6527faacfe86a8b9a00a6956',
-          name: 'Главная',
-          handle: '/',
-          description: '',
-          media: [],
-          children: [],
-          code: 'footer_nav_home',
-          __v: 0
-        },
-        {
-          _id: '65282729231262fb09808d7f',
-          name: 'Каталог',
-          handle: '/shop',
-          description: null,
-          media: [],
-          children: [],
-          code: 'footer_nav_cat',
-          __v: 0
-        },
-        {
-          _id: '65282746231262fb09808d97',
-          name: 'Корзина',
-          handle: '/pages/cart',
-          description: null,
-          media: [],
-          children: [],
-          code: 'footer_nav_cart',
-          __v: 0
-        },
-        {
-          _id: '65282773231262fb09808dad',
-          name: 'Блог',
-          handle: '/blog',
-          description: null,
-          media: [],
-          children: [],
-          code: 'footer_nav_blog',
-          __v: 0
-        },
-        {
-          _id: '652827b3231262fb09808dd2',
-          name: 'Контакты',
-          handle: '/pages/contact-us',
-          description: null,
-          media: [],
-          children: [],
-          code: 'footer_nav_cont',
-          __v: 0
-        },
-        {
-          _id: '6565a39cb334bb89c2daa450',
-          name: 'Политика обработки персональных данных',
-          handle: '/pages/privacy',
-          description: null,
-          media: [],
-          children: [],
-          code: 'footer_nav_privacy',
-          __v: 0
-        },
-        {
-          _id: '6565a407b334bb89c2daa47c',
-          name: 'Договор публичной оферты',
-          handle: '/pages/offer',
-          description: null,
-          media: [],
-          children: [],
-          code: 'footer_nav_offer',
-          __v: 0
-        },
-        {
-          _id: '6683d846ee4a005796633aff',
-          name: 'Условия оплаты',
-          handle: '/pages/payment-terms/',
-          description: null,
-          media: [],
-          children: [],
-          code: 'nav_footer_payment_terms',
-          __v: 0
-        },
-        {
-          _id: '6683d886ee4a00579663435b',
-          name: 'Условия доставки',
-          handle: '/pages/delivery-terms',
-          description: null,
-          media: [],
-          children: [],
-          code: 'nav_footer_delivery_terms',
-          __v: 0
-        }
-      ],
-      code: 'footer_nav',
-      __v: 0
-    },
-    {
-      address: 'г. Минск',
-      copyright: ' Mac Plus',
-      email: 'info@macplus.by',
-      instagram: 'https://www.instagram.com/macplus.by/',
-      legal: 'ООО "МакПлюсТрейд"\n' +
-        'УНП 491468179\n' +
-        'Дата регистрации в торговом реестре - 10.11.2023.',
-      'main-seo-description': 'Продажа Электроники / Гаджетов в Минске и Беларуси',
-      'main-seo-title': 'Интернет-магазин электроники в Беларуси',
-      'nav-limit': '7',
-      'nav-sale-image': '220c78c519b86b.webp',
-      'nav-sale-link': 'https://macplus.by/macbook/MacBook_Pro_M3',
-      'nav-sale-title': 'Купить сейчас',
-      phone: '+375 (29) 644 40 42',
-      telegram: 'https://t.me/macplusby',
-      viber: '+375296444042',
-      work_time: 'Пн - Вс / 9:00 - 20:00'
-    }
-  ]
+  ] = await Promise.all([
+    getCategoryTree(),
+    getMenuByCode("footer_nav"),
+    getFieldsObject(
+      "telegram",
+      "viber",
+      "instagram",
+      "phone",
+      "email",
+      "address",
+      "work_time",
+      "copyright",
+      "legal",
+      "nav-sale-title",
+      "nav-sale-link",
+      "nav-sale-image",
+      "main-seo-title",
+      "main-seo-description",
+      "nav-limit"
+    )
+  ]);
   let pageProps = {};
   if (Component.getInitialProps) {
     const pagePropsRes = await Component.getInitialProps(ctx);
