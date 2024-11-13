72b2517b47b818fc178f7d715eecafe8980603e0
zidiks <zidiks228@gmail.com>
2024-11-13 11:06:29 +0300

    fix: schematic + remove img magnifier

--- components/partials/shop/sidebar/sidebar-filter-one.jsx
+++ components/partials/shop/sidebar/sidebar-filter-one.jsx
@@ -116,8 +116,8 @@ export default function SidebarFilterOne({ type = 'left', isFeatured = false, fi
       <div className="sidebar-overlay" onClick={hideSidebar}></div>
       {type === 'boxed' || type === 'banner' ? (
         <a href="#" className="sidebar-toggle" onClick={showSidebar}>
-          <svg width={24} height={24} xmlns="http://www.w3.org/2000/svg" class="ionicon" viewBox="0 0 512 512">
-            <path d="M35.4 87.12l168.65 196.44A16.07 16.07 0 01208 294v119.32a7.93 7.93 0 005.39 7.59l80.15 26.67A7.94 7.94 0 00304 440V294a16.07 16.07 0 014-10.44L476.6 87.12A14 14 0 00466 64H46.05A14 14 0 0035.4 87.12z" fill="none" stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="32" />
+          <svg width={24} height={24} xmlns="http://www.w3.org/2000/svg" className="ionicon" viewBox="0 0 512 512">
+            <path d="M35.4 87.12l168.65 196.44A16.07 16.07 0 01208 294v119.32a7.93 7.93 0 005.39 7.59l80.15 26.67A7.94 7.94 0 00304 440V294a16.07 16.07 0 014-10.44L476.6 87.12A14 14 0 00466 64H46.05A14 14 0 0035.4 87.12z" fill="none" stroke="currentColor" strokeLinecap="round" strokeLinejoin="round" strokeWidth="32" />
           </svg>
         </a>
       ) : (
