06f0fbe5d8c54bb7cf52f2a8d85fb6e17dfbab8a
zidiks <zidiks228@gmail.com>
2025-08-12 14:55:10 +0300

    upd: guarantee banner

--- components/partials/home/banner-section.jsx
+++ components/partials/home/banner-section.jsx
@@ -1,42 +1,30 @@
 import React from 'react';
 import Reveal from 'react-awesome-reveal';
-import recycleImage from '~/public/images/recycle.png';
-import ALink from '~/components/features/custom-link';
+import gearImage from '~/public/images/gear.svg';
+import guyImage from '~/public/images/guy.png';
 import {fadeIn} from '~/utils/data/keyframes';
 import Image from 'next/image';
-import InlineSVG from "react-inlinesvg";
-import {chevronForwardOutlineIcon} from "~/icons/chevron-forward-outline";
+import ALink from "~/components/features/custom-link";
 
 function BannerSection({ tradeInTitle, tradeInSubtitle, tradeInDescription }) {
   return (
-    <section className="banner-group container mt-10 pb-4 pt-2 mb-10">
-      <Reveal keyframes={fadeIn} delay={200} duration={1200} triggerOnce>
-        <div className="banner1 banner" style={{ backgroundColor: '#007945', overflow: 'hidden', borderRadius: '2rem' }}>
-          <div className="banner-content">
-            <div>
-              <Image src={recycleImage} alt="Trade-in" title="Trade-in" width={60} height={60} loading="lazy" className="banner-icon mb-4" />
-            </div>
-            <span className="banner-title text-white ls-normal lh-1">{tradeInTitle}</span>
-            <span className="banner-subtitle text-white ls-normal lh-1">{tradeInSubtitle}</span>
-            <p className="mb-7 text-white">{tradeInDescription}</p>
-            <div className="banner-links">
-              <ALink href="/pages/trade-in" className="btn btn-link btn-white btn-underline font-weight-bold">
-                <div className="btn-content">
-                  <span>Подробнее</span>
-                  <InlineSVG className="icon-18 btn-content-icon" src={chevronForwardOutlineIcon}/>
-                </div>
-              </ALink>
-              <ALink href="/pages/contact-us" className="btn btn-link btn-white btn-underline font-weight-bold">
-                <div className="btn-content">
-                  <span>Связаться</span>
-                  <InlineSVG className="icon-18 btn-content-icon" src={chevronForwardOutlineIcon}/>
-                </div>
-              </ALink>
+    <ALink href="pages/warranty">
+      <section className="banner-group container mt-10 pb-4 pt-2 mb-10">
+        <Reveal keyframes={fadeIn} delay={200} duration={1200} triggerOnce>
+          <div className="banner1 banner banner-section-wrapper" style={{ overflow: 'hidden', borderRadius: '2rem' }}>
+            <Image src={guyImage} alt="guarantee" title="guarantee" width={360} height={426} loading="lazy" className="banner-guy" />
+            <div className="banner-content">
+              <div>
+                <Image src={gearImage} alt="guarantee" title="guarantee" width={60} height={60} loading="lazy" className="banner-icon mb-4" />
+              </div>
+              <span className="banner-title text-white ls-normal lh-1">{'На всю технику' || tradeInTitle}</span>
+              <span className="banner-title text-white ls-normal lh-1">{'предоставляем ГАРАНТИЮ 1 год' || tradeInSubtitle}</span>
+              <span className="banner-title text-white ls-normal lh-1">{'и 36 месцев сервисного обслуживания' || tradeInDescription}</span>
             </div>
           </div>
-        </div>
-      </Reveal>
-    </section>
+        </Reveal>
+      </section>
+    </ALink>
   );
 }
 
