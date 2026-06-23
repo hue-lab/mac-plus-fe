91eef45a20121fa8ed651e31f94001f6040a0bb4
Zidiks <zidiks228@gmail.com>
2026-06-23 21:20:39 +0300

    upd: spam bot guard

--- utils/endpoints/message.js
+++ utils/endpoints/message.js
@@ -1,8 +1,20 @@
+async function getPublicFormToken(action) {
+  const res = await fetch(process.env.API_HOST + `/security/public-form-token/${action}`);
+
+  if (!res.ok) {
+    throw new Error('Cannot get form token');
+  }
+
+  const data = await res.json();
+  return data.token;
+}
+
 export async function sendMessage({name, phone, message, website = '', turnstileToken}) {
+  const formToken = await getPublicFormToken('quick-message');
   const res = await fetch(process.env.API_HOST + '/notify/message', {
     method: "POST",
     headers: { 'Content-Type': 'application/json' },
-    body: JSON.stringify({name, phone, message, website, turnstileToken}),
+    body: JSON.stringify({name, phone, message, website, turnstileToken, formToken}),
   });
 
   if (!res.ok) {
--- utils/endpoints/orders.js
+++ utils/endpoints/orders.js
@@ -8,12 +8,24 @@ export async function getPaymentMethods() {
   return res.json() || [];
 }
 
+async function getPublicFormToken(action) {
+  const res = await fetch(process.env.API_HOST + `/security/public-form-token/${action}`);
+
+  if (!res.ok) {
+    throw new Error('Cannot get form token');
+  }
+
+  const data = await res.json();
+  return data.token;
+}
+
 export async function addOrder(obj) {
+  const formToken = await getPublicFormToken('order');
   const res = await fetch(process.env.API_HOST + '/store/order', {
     method: 'POST',
     headers: { 'Content-Type': 'application/json' },
     body: JSON.stringify(
-      obj
+      {...obj, formToken}
     )
   })
   return await res.json();
